1604cb3e638e6d049f90db7308ec65f7a4e4777d
RaY8118 <parthghadi43@gmail.com>
2024-01-28 14:43:50 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import cv2
+import pickle
+import numpy as np
+import face_recognition
+import cvzone
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+imgBackground = cv2.imread('Resources/background.png')
+file = open('Resources/EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    k = cv2.waitKey(1)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print("matches", matches)
+        print("faceDis", faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            print("Known face detected")
+            student_id = studentIds[matchIndex]  # ID from face recognition
+            print(student_id)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+            cv2.putText(imgBackground, student_id, (bbox[0], bbox[1] - 35), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                        (255, 255, 0), 3, lineType=cv2.LINE_AA)
+        
+    if k == ord('q'):
+        break
+    cv2.waitKey(1)
